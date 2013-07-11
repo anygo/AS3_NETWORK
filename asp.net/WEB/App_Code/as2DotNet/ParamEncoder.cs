f5f6277778bbb9644644a3f833e3d8671a899162
dawn <295283480@qq.com>
2013-07-11 16:09:54 +0800

    完善encoder

--- asp.net/WEB/App_Code/as2DotNet/ParamEncoder.cs
+++ asp.net/WEB/App_Code/as2DotNet/ParamEncoder.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.IO;
+using System.Text;
 
 namespace as2DotNet
 {
@@ -32,11 +33,98 @@ namespace as2DotNet
 	     */
 	    public byte[] encode(DataVO data)
 	    {
-            byte[] resultData = new byte[10];
-            //BufferedStream bs = new BufferedStream();
-            Stream stream = new MemoryStream(data);
-            BinaryWriter reader = new BinaryWriter(stream, Encoding.UTF8);
-            return null;
+            MemoryStream stream = new MemoryStream();
+            BinaryWriter writer = new BinaryWriter(stream);
+
+            writeUTF(writer, data.ResultStatus);
+            if (data.ResultStatus == "error")
+            {
+                writeUTF(writer, data.ErrorMessage);
+            }
+            else
+            {
+                writeUTF(writer, data.ServiceName);
+                writeUTF(writer, data.MethodName);
+                writeAll(writer, data.Result);
+            }
+
+            return stream.GetBuffer();
+        }
+
+        public void writeAll(BinaryWriter writer, object data)
+        {
+            if (data == null)
+            {
+                writeByte(writer, ParamType.NULL);
+            }
+            else if (outEncoder != null && outEncoder.encode(data, writer))
+            {
+                return;
+            }
+            else if (data is string)
+            {
+                writeByte(writer, ParamType.STRING);
+                writeUTF(writer, (string)data);
+            }
+
+        }
+
+        public void writeUTF(BinaryWriter writer, string value)
+        {
+            long startPos = writer.BaseStream.Position;
+            writeUnsignedShort(writer, 0);
+            writeUTFBytes(writer, value);
+            long endPos = writer.BaseStream.Position;
+            ushort len = (ushort)(endPos - startPos - 2);
+            writer.BaseStream.Position = startPos;
+            writeUnsignedShort(writer, len);
+            writer.BaseStream.Position = endPos;
+        }
+
+        public void writeUTFBytes(BinaryWriter writer, string value)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(value);
+            writer.Write(bytes);
+        }
+
+        public void writeBoolean(BinaryWriter writer, bool value)
+        {
+            writer.Write(value);
+        }
+
+        public void writeByte(BinaryWriter writer, byte value)
+        {
+            writer.Write(value);
+        }
+
+        public void writeBytes(BinaryWriter writer, byte[] value)
+        {
+            writer.Write(value);
+        }
+
+        public void writeDouble(BinaryWriter writer, double value)
+        {
+            writer.Write(value);
+        }
+
+        public void writeFloat(BinaryWriter writer, float value)
+        {
+            writer.Write(value);
+        }
+
+        public void writeInt(BinaryWriter writer, int value)
+        {
+            writer.Write(value);
+        }
+
+        public void writeShort(BinaryWriter writer, short value)
+        {
+            writer.Write(value);
+        }
+
+        public void writeUnsignedShort(BinaryWriter writer, ushort value)
+        {
+            writer.Write(value);
         }
     }
 }
